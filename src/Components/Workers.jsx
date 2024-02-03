862f36fda98c278c6d55e3bc35ac7e4681712dea
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2024-02-03 23:10:50 +0400

    updated

--- src/Components/Workers.jsx
+++ src/Components/Workers.jsx
@@ -100,7 +100,7 @@ const Workers = () => {
       location: location || null,
     };
   
-    fetch('http://localhost:5000/api/saveWorkingTime', {
+    fetch('https://tnapp.onrender.com/api/saveWorkingTime', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
