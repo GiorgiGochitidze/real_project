a480e274df21c7e9c1836aff8e6571db23a44b38
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2024-02-05 15:45:50 +0400

    timer and map update

--- src/Components/UserLocationsMap.jsx
+++ src/Components/UserLocationsMap.jsx
@@ -13,37 +13,29 @@ const UserLocationsMap = () => {
     popupAnchor: [0, -32],
   });
 
-  const getCurrentLocation = () => {
-    // Fetch the user's current location
+  const handlePositionChange = (position) => {
+    const { latitude, longitude } = position.coords;
+    setCurrentLocation({ latitude, longitude });
+    console.log('Current Location:', { latitude, longitude });
+  };
+
+  useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          setCurrentLocation({ latitude, longitude });
-          console.log('Current Location:', { latitude, longitude });
-        },
+      // Set up a continuous watch on the user's position
+      const watchId = navigator.geolocation.watchPosition(
+        handlePositionChange,
         (error) => {
           console.error('Error getting location:', error.message);
         },
         { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
       );
+
+      // Clear the watch on component unmount
+      return () => navigator.geolocation.clearWatch(watchId);
     } else {
       console.error('Geolocation is not supported by this browser.');
     }
-  };
-
-  useEffect(() => {
-    // Fetch and update current location initially
-    getCurrentLocation();
-
-    // Set up an interval to fetch and update the location every 3 seconds
-    const intervalId = setInterval(() => {
-      getCurrentLocation();
-    }, 900);
-
-    // Clear the interval on component unmount
-    return () => clearInterval(intervalId);
-  }, []); // Empty dependency array ensures this effect runs only once
+  }, []);
 
   if (!currentLocation) {
     return <p>Loading current location...</p>;
